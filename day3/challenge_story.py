a95d33af75845647a54004c5eeb01cf10bb0440c
victor magdaleno <vrmq@gft.com>
2023-04-20 16:34:20 -0600

    resolve banker roulette

--- day3/challenge_story.py
+++ day3/challenge_story.py
@@ -39,6 +39,46 @@ choose_l3 =input(print('All the team coverages from damaged inside the lab and s
 
 choose_l4 = input(print('Discover organic resources and water. Decide to create a new civilization beside Earth. \n Enter the new name  for your Civilization')).lower()
 
+new_planet = print('''
+                   
+                                                                                         ..;===+.
+                                                                  .:=iiiiii=+=
+                                                               .=i))=;::+)i=+,
+                                                            ,=i);)I)))I):=i=;
+                                                         .=i==))))ii)))I:i++
+                                                       +)+))iiiiiiii))I=i+:'
+                                  .,:;;++++++;:,.       )iii+:::;iii))+i='
+                               .:;++=iiiiiiiiii=++;.    =::,,,:::=i));=+'
+                             ,;+==ii)))))))))))ii==+;,      ,,,:=i))+=:
+                           ,;+=ii))))))IIIIII))))ii===;.    ,,:=i)=i+
+                          ;+=ii)))IIIIITIIIIII))))iiii=+,   ,:=));=,
+                        ,+=i))IIIIIITTTTTITIIIIII)))I)i=+,,:+i)=i+
+                       ,+i))IIIIIITTTTTTTTTTTTI))IIII))i=::i))i='
+                      ,=i))IIIIITLLTTTTTTTTTTIITTTTIII)+;+i)+i`
+                      =i))IIITTLTLTTTTTTTTTIITTLLTTTII+:i)ii:'
+                     +i))IITTTLLLTTTTTTTTTTTTLLLTTTT+:i)))=,
+                     =))ITTTTTTTTTTTLTTTTTTLLLLLLTi:=)IIiii;
+                    .i)IIITTTTTTTTLTTTITLLLLLLLT);=)I)))))i;
+                    :))IIITTTTTLTTTTTTLLHLLLLL);=)II)IIIIi=:
+                    :i)IIITTTTTTTTTLLLHLLHLL)+=)II)ITTTI)i=
+                    .i)IIITTTTITTLLLHHLLLL);=)II)ITTTTII)i+
+                    =i)IIIIIITTLLLLLLHLL=:i)II)TTTTTTIII)i'
+                  +i)i)))IITTLLLLLLLLT=:i)II)TTTTLTTIII)i;
+                +ii)i:)IITTLLTLLLLT=;+i)I)ITTTTLTTTII))i;
+               =;)i=:,=)ITTTTLTTI=:i))I)TTTLLLTTTTTII)i;
+             +i)ii::,  +)IIITI+:+i)I))TTTTLLTTTTTII))=,
+           :=;)i=:,,    ,i++::i))I)ITTTTTTTTTTIIII)=+'
+         .+ii)i=::,,   ,,::=i)))iIITTTTTTTTIIIII)=+
+        ,==)ii=;:,,,,:::=ii)i)iIIIITIIITIIII))i+:'
+       +=:))i==;:::;=iii)+)=  `:i)))IIIII)ii+'
+     .+=:))iiiiiiii)))+ii;
+    .+=;))iiiiii)));ii+
+   .+=i:)))))))=+ii+
+  .;==i+::::=)i=;
+  ,+==iiiiii+,
+  `+=+++;`
+''')
+
 print(choose_l1)
 
 if choose_l1 == 'up':
@@ -60,3 +100,4 @@ if choose_l3 == 'up':
     choose_l4
 
 print(choose_l4)
+print(new_planet)
---
+++ day4/lists.py
@@ -0,0 +1,11 @@
+tech_companies = ['Apple', 'Netflix', 'Microsoft', 'Facebook', 'Alphabet', ]
+
+tech_companies[1] = 'Uber'
+
+print(tech_companies[1])
+
+tech_companies.append('Hanna Barbera')
+tech_companies.extend(['Angela Land', 'Raven Land'])
+
+print(tech_companies)
+
