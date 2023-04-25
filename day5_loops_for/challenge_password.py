e91c02b7740e829f4ce45a7044cfecc2d52d069d
Victor Izquierdo <vhmizq@gmail.com>
2023-04-24 19:41:41 -0600

    finished hard level password generator

--- day5_loops_for/challenge_password.py
+++ day5_loops_for/challenge_password.py
@@ -13,35 +13,36 @@ length_letter = len(letters)
 length_symbols = len(symbols)
 length_number = len(numbers)
 
-
-
-
-
-
-
 letter_str = ''
 number_str = ''
 symbol_str = ''
+
+# Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
 
 for char in range(1, (nr_letters + 1)):
   number_letter = random.randint(1, (length_letter - 1))
   random_letter = letters[number_letter]
   letter_str += random_letter
-print(letter_str)
 
 for char in range(1, (nr_numbers + 1)):
   number_number = random.randint(1, (length_number - 1))
   random_number = numbers[number_number]
   number_str += random_number
-print(number_str)
 
 for char in range(1, (nr_symbols + 1)):
-  number_symbols = random.randint(1, (length_symbols - 1)) 
-  random_symbols = symbols[number_symbols]
-  symbol_str += random_symbols
-print(symbol_str)
+  symbol_str += random.choice(symbols)
+  
+password = letter_str + number_str + symbol_str
 
-print(f'Here is your password: {letter_str}{number_str}{symbol_str}')
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+print(f'Here is your password: {password}')
+# Hard Level - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
 
+random_password = ''
+string_list = list(password)
+random.shuffle(string_list)
+
+for char in string_list:
+  random_password += char
+print(f'Here is your Fort Knox password: {random_password}')
