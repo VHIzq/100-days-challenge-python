b4a90d87e15ed39f53b8ddcdeba06b28da7b93b4
vhmi <vhmizq@gmail.com>
2023-04-27 20:59:52 -0600

    finished app

--- day7_hangman/task1.py
+++ day7_hangman/task1.py
@@ -1,48 +1,42 @@
-# Step 1
-
-
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
-
-
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
-
-# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
-
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-
-#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
-
 import random
-word_list = ["aardvark", "baboon", "camel"]
+import stages
+import logo
+import hangman_word
 
-chosen_word = random.choice(word_list)
-print(chosen_word)
-
-guess_list = list(chosen_word)
-display = []
-blanks_count = 0
-
-while blanks_count == 0:
-  guess = input('Try to guess a letter from the word \n').lower()
-  print(guess)
-  for char in guess_list:
-    isMatch = char == guess
-
-  if isMatch:
-    print('matches!')
-    display.append(guess)
-  else:
-    print('Shot!')
-    display.append('_')
-      
-blanks_count = display.count('_')
-
-
+lives = 6
+chosen_word = random.choice(hangman_word.word_list)
+end_game = False
+display = ['_'] * len(chosen_word)
+logo = logo.logo
+print(logo)
 print(display)
-print(blanks_count)
+print(stages.stages[6])
 
+while not end_game:
+  guess = input('Guess a letter\n').lower()
+  
+  if guess in display:
+      print(f'The letter {guess} is already entered')  
+  
+  for index, char in enumerate(chosen_word):
+    isMatch = char == guess
+    if isMatch:
+      display[index] = guess
+        
+  if guess not in chosen_word:
+    lives -= 1
+    print(stages.stages[lives])
+    print(lives)
+    print(f'The letter {guess} is not in random word')
+  
+  print(display)
+  
+  has_blanks = '_' not in display
+  if has_blanks:
+    end_game = True
+    print('U WIN!')
+  
+  is_die = lives == 0
+  if is_die:
+    end_game = True
+    print('U LOSE!')
