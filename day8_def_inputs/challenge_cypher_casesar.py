c2ec432fa87f0c35f290b7ddfd948ffcedbd4784
Victor Izquierdo <vhmizq@gmail.com>
2023-04-29 19:26:33 -0600

    missing ux

--- day8_def_inputs/challenge_cypher_casesar.py
+++ day8_def_inputs/challenge_cypher_casesar.py
@@ -1,29 +1,48 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+            'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-#direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
-#shift = int(input("Type the shift number:\n"))
+shift = int(input("Type the shift number:\n"))
 
-# TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-def encrypt(text):
-
-# TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.
-
+def caesar(text, shift, direction):
+  plain_text = ''
+  forward = True
+  
   for letter in text:
-    #print(letter)
-    alphabet[]
-        
-encrypt(text)
-# e.g.
-# plain_text = "hello"
-# shift = 5
-# cipher_text = "mjqqt"
-# print output: "The encoded text is mjqqt"
+    if direction.lower() == 'encode':
+      new_position = alphabet.index(letter) + shift
+    elif direction.lower() == 'decode':
+      new_position = alphabet.index(letter) - shift
+      forward = False
 
-# HINT: How do you get the index of an item in a list:
-# https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+    new_char = alphabet[new_position]
+    plain_text += new_char
 
-# 🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+  if forward:
+    print(f'Crypted message is {plain_text}')
+  else:
+    print(f'The decoded text is {plain_text} ')
 
-# TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message.
+caesar(text, shift, direction)
+
+# TODO-1: Import and print the logo from art.py when the program starts.
+
+# TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
+# Try running the program and entering a shift number of 45.
+# Add some code so that the program continues to work even if the user enters a shift number greater than 26.
+# Hint: Think about how you can use the modulus (%).
+
+# TODO-3: What happens if the user enters a number/symbol/space?
+# Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
+# e.g. start_text = "meet me at 3"
+# end_text = "•••• •• •• 3"
+
+# TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
+# e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
+# If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
+# Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+
+
+
