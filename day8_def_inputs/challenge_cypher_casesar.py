ae9bc40ea9240bb3ab9900e383ba318cf7b814be
victor magdaleno <vrmq@gft.com>
2023-05-02 14:39:58 -0600

    finished cypher

--- day8_def_inputs/challenge_cypher_casesar.py
+++ day8_def_inputs/challenge_cypher_casesar.py
@@ -5,61 +5,46 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
             'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
 print(logo.logo)
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
 
-play_again = False
+
+play_again = True
 
 #ask_again = input('Do you want to play again? type yes or not').lower()
 
 def caesar(text, shift, direction):
   plain_text = ''
-  forward = True
-  
+  forward = False
+  shift = shift % 26
   for letter in text:
-    if direction.lower() == 'encode':
-      new_position = alphabet.index(letter) + shift
-    elif direction.lower() == 'decode':
-      new_position = alphabet.index(letter) - shift
-      forward = False
-
-    new_char = alphabet[new_position]
-    plain_text += new_char
-
+    if letter in alphabet:
+      if direction.lower() == 'encode':
+          new_position = alphabet.index(letter) + shift
+      elif direction.lower() == 'decode':
+        new_position = alphabet.index(letter) - shift
+        forward = False
+      new_char = alphabet[new_position]
+      plain_text += new_char
+    
+    else:
+      plain_text += letter
+      
   if forward:
     print(f'Crypted message is {plain_text}')
   else:
     print(f'The decoded text is {plain_text}')
 
+while play_again:
+  
+  direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+  text = input("Type your message:\n").lower()
+  shift = int(input("Type the shift number:\n"))
+  caesar(text, shift, direction)
 
-def check_shift():
-  if not shift > 26 or shift != 26:
-    caesar(text, shift, direction)
-  else:
-    print('Please, enter a shift minus than 26')
-check_shift()
-""" if ask_again == 'yes':
-  play_again = True
-  while play_again:
-    check_shift() """
-
-# TODO-1: Import and print the logo from art.py when the program starts.
-
-# TODO-2: What if the user enters a shift that is greater than the number of letters in the alphabet?
-# Try running the program and entering a shift number of 45.
-# Add some code so that the program continues to work even if the user enters a shift number greater than 26.
-# Hint: Think about how you can use the modulus (%).
-
-# TODO-3: What happens if the user enters a number/symbol/space?
-# Can you fix the code to keep the number/symbol/space when the text is encoded/decoded?
-# e.g. start_text = "meet me at 3"
-# end_text = "•••• •• •• 3"
-
-# TODO-4: Can you figure out a way to ask the user if they want to restart the cipher program?
-# e.g. Type 'yes' if you want to go again. Otherwise type 'no'.
-# If they type 'yes' then ask them for the direction/text/shift again and call the caesar() function again?
-# Hint: Try creating a while loop that continues to execute the program if the user types 'yes'.
+  game_again = input('Do you want to play again? Type "yes" or "no"')
+  
+  if game_again == 'no':
+    play_again = False
+    print('Goodbye')
 
 
 
