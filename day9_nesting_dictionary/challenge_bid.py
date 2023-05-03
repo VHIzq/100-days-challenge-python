b911e423241c7be1d5bf77cf35d62f7ba6077e83
vhmi <vhmizq@gmail.com>
2023-05-02 20:03:15 -0600

    auction challenges finished with def

--- day9_nesting_dictionary/challenge_bid.py
+++ day9_nesting_dictionary/challenge_bid.py
@@ -4,6 +4,17 @@ print(logo.logo)
 more_auctions = True
 auction = {}
 
+def find_highest_bid(bidding_record):
+  hightest_bid = 0
+  winner = ''
+  
+  for bidder in bidding_record:
+    bidder_anmount = bidding_record[bidder]
+    if bidder_anmount > hightest_bid:
+      hightest_bid = bidder_anmount
+      winner = bidder
+  print(f'The winner is {winner} with a BID of {hightest_bid}.')
+
 while more_auctions:
   name = input('Introduce yourn name: ')
   bid = int(input('What is your BID: $'))
@@ -12,8 +23,9 @@ while more_auctions:
 
   if more_people == 'no':
     more_auctions = False
+    find_highest_bid(auction)
     
-    win_bidder = max([i for i in auction.values()])
+    """ win_bidder = max([i for i in auction.values()])
     win_name = max(auction, key=auction.get)
 winner = f'THe winner is {win_name} with a BID of {win_bidder} '
-print(winner)
+print(winner) """ 
