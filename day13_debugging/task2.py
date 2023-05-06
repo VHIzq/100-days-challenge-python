342b184341670a0133f4fd87b1e9037a5fa113b9
Victor Izquierdo <vhmizq@gmail.com>
2023-05-06 14:28:39 -0600

    high vs lower challenge add

--- day13_debugging/task2.py
+++ day13_debugging/task2.py
@@ -1,14 +1,13 @@
 year = int(input("Which year do you want to check?"))
-type(year)
 
-# if year % 4 == 0:
-#   if year % 100 == 0:
-#     if year % 400 == 0:
-#       print("Leap year.")
-#     else:
-#       print("Not leap year.")
-#   else:
-#     print("Leap year.")
-# else:
-#   print("Not leap year.")
+if year % 4 == 0:
+  if year % 100 == 0:
+    if year % 400 == 0:
+      print("Leap year.")
+    else:
+      print("Not leap year.")
+  else:
+    print("Leap year.")
+else:
+  print("Not leap year.")
   
