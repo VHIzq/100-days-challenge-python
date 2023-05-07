b8de31241bafc3d441897a140c3a1a2281a94d08
vhmi <vhmizq@gmail.com>
2023-05-06 20:38:19 -0600

    working followers

--- day14_high_lower/challenge.py
+++ day14_high_lower/challenge.py
@@ -3,11 +3,6 @@ from art import logo, vs
 from game_data import data
 
 print(logo)
-""" 'name': 'Instagram',
-        'follower_count': 346,
-        'description': 'Social media platform',
-        'country': 'United States'
-"""
 
 #create generate_card to show the name, details and country from each question
 number_a = random.choice(data)
@@ -21,7 +16,7 @@ def generate_card(random_number):
   return f'{card_name} is a {card_description} from {card_country}'
 
 card_a = f'Compare A: {generate_card(random_number=number_a)}'
-card_b =f'Against B: {generate_card(random_number=number_b)}'
+card_b = f'Against B: {generate_card(random_number=number_b)}'
 
 followers_a = number_a['follower_count']
 followers_b = number_b['follower_count']
@@ -39,18 +34,21 @@ asking = input('Who has more followers? Type "A" or "B": ').upper()
 def comparator():
   is_a_greater = followers_a > followers_b
   is_b_greater = followers_b > followers_a
-  if asking == 'A':
+  response_a = asking == 'A'
+  response_b = asking == 'B'
+  
+  if response_a:
     if is_a_greater:
       print('You are right!')
     else:
       print('You are wrong!')
-  elif asking == 'B':
+  elif response_b:
     if is_b_greater:
       print('You are right!')
     else:
       print('You are wrong!')
   else:
-    print('Introduce a valid option')
+    print('Introduce a valid option: ')
     
 comparator()
 #If option selected is true, then this is going to be compared to new question. Don't game over
